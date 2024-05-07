ba680036491932b8f38a00022807b73e32a5be3f
mrcsbin <mrcsbin@naver.com>
2024-05-07 14:29:23 +0900

    chore: Initial commit

---
+++ src/main/java/com/tinyhappytrip/TinyHappyTripApplication.java
@@ -0,0 +1,13 @@
+package com.tinyhappytrip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinyHappyTripApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TinyHappyTripApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tinyhappytrip/TinyHappyTripApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tinyhappytrip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TinyHappyTripApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
