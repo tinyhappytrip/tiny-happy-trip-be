73435612eb273272f8725d5f4614a191f6e7ba0e
mrcsbin <mrcsbin@naver.com>
2024-05-08 16:01:49 +0900

    feat: 소셜 로그인 기능 추가 - 네이버, 구글, 카카오 로그인 - 계정 연동 및 연동 해제

---
+++ src/main/java/com/tinyhappytrip/oauth2/exception/OAuth2AuthenticationProcessingException.java
@@ -0,0 +1,7 @@
+package com.tinyhappytrip.oauth2.exception;
+
+public class OAuth2AuthenticationProcessingException extends ArithmeticException {
+    public OAuth2AuthenticationProcessingException(String msg) {
+        super(msg);
+    }
+}
