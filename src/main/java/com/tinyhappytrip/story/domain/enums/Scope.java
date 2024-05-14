37721b4f4984fa91b9168529ef60f0b360432d21
sumin305 <gogotnals@naver.com>
2024-05-15 00:25:40 +0900

    chore: 깃 충돌 해결

--- src/main/java/com/tinyhappytrip/story/domain/enums/Scope.java
+++
@@ -1,7 +0,0 @@
-package com.tinyhappytrip.story.domain.enums;
-
-public enum Scope {
-    PUBLIC,
-    PRIVATE,
-    FOLLOWER
-}
--- src/main/java/com/tinyhappytrip/user/domain/enums/SocialType.java
+++
@@ -1,8 +0,0 @@
-package com.tinyhappytrip.user.domain.enums;
-
-public enum SocialType {
-    GOOGLE,
-    KAKAO,
-    NAVER,
-    EMAIL
-}
